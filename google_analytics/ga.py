38a7fdd3e0895eb4a1e242d783fcb509bde1e3cb
Ilia <phanoix@gmail.com>
2023-08-01 15:37:02 -0400

    Update ga.py - api query + response handling

--- google_analytics/ga.py
+++ google_analytics/ga.py
@@ -13,9 +13,35 @@ class gcga_v2:
             "gcPedia": "ga:39673253",
             "gcWiki": "ga:177027196"
         }
+        self.platformsGA4 =  {
+            "gcCollab": "properties/383884917",
+            "gcConnex": "properties/383883780",
+            "gcPedia": "properties/383864912",
+            "gcWiki": "properties/362244995"
+        }
     
     def get_platform_metrics(self, platform="gccollab", startDate='2019-04-01', endDate='2019-06-26'):
         #print(startDate, endDate)
+        if (startDate >= '2023-07-01'):
+            return self.analyticsGA4.properties().batchRunReports(
+                property = self.platformsGA4[platform],
+                body={
+                'requests': [
+                {
+                    'dateRanges': [{'startDate': startDate, 'endDate': endDate}],
+                    'metrics': [
+                        {'expression': 'screenPageViews', 'name': 'pageviews'},
+                        {'expression': 'totalUsers', 'name': 'users'},  # or activeUsers
+                        {'expression': 'averageSessionDuration', 'name': 'avgSessionDuration'},
+                        {'expression': 'bounceRate', 'name': 'bounce'}, # (Sessions Minus Engaged sessions) divided by Sessions), where an engaged session lasted “longer than 10 seconds, or had a conversion event, or had 2 or more screen views.”
+                        {'expression': '', 'name': 'averagePageLoadTime'},  # not a GA4 metric
+                        {'expression': 'sessions', 'name': 'numsessions'},
+                        {'expression': 'screenPageViewsPerSession', 'name': 'pageviewsPerSession'}
+                    ]
+                }]
+                }
+            ).execute()
+            
         return self.analytics.reports().batchGet(
             body={
             'reportRequests': [
@@ -56,7 +82,8 @@ class gcga_v2:
                 prev_value = "N/A"
 
             try:
-                current_value = round (float(current_response["reports"][0]["data"]["rows"][0]["metrics"][0]["values"][x]), 2)
+                #current_value = round (float(current_response["reports"][0]["data"]["rows"][0]["metrics"][0]["values"][x]), 2)
+                current_value = round (float(current_response["reports"][0]["rows"][0]["metricValues"][x]["value"]), 2)            # either this will need to be handled based on response type or in about a year it can simply completely replace the old response handling
                 if current_value == int(current_value):
                     current_value = int(current_value)
             except:
